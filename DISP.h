e40a870e693aff0c6f98786598b11fae974b2cbc
Jeremy Shen <ly0koS@hotmail.com>
2018-12-30 16:56:30 +0800

    Reupload

--- DISP.h
+++
@@ -1,8 +0,0 @@
-#ifndef _DISP_
-#define _DISP_
-#include <reg52.h>
-#include <intrins.h>
-#define uchar unsigned char
-void delay(uchar ti);
-void display(d1,d2);
-#endif
--- Func.h
+++
@@ -1,7 +0,0 @@
-#ifndef _Func_
-#define _Func_
-#define uchar unsigned char
-void Calculate(uchar c);
-void write24c02(void);
-uchar read24c02(void);
-#endif
