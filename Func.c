84e391608d031ee0e6bab49e649bf9e92b7dd895
Jeremy Shen <ly0koS@hotmail.com>
2018-12-30 19:14:04 +0800

    write24c02

--- Func.c
+++ Func.c
@@ -1,6 +1,8 @@
 #include "Func.h"
 #define uchar unsigned char
-	
+
+sbit erflags=PSW^5;
+
 extern data uchar  param[9];
 
 void Calculate(uchar c)
@@ -21,5 +23,19 @@ uchar read24c02(void)
 }
 void write24c02(void)
 {
+	uchar i;
+	sen:	erflags=0;						  
+		Start( ); 							
+		SendByte(0x90); 				
+		cAck( );							
+		if(erflags==1) goto sen;			
+		SendByte(0x00); 					
+		cAck( );							
+		if(erflags==1) goto sen;	
+	for(i=0;i<8;i++)
+	{
+		SendByte(param[i]); 						
+		cAck( );
+	}
 	
 }
