6bbbf7bde99dfdc691104c0d6f82d077ff633044
Jeremy Shen <ly0koS@hotmail.com>
2018-12-30 17:11:17 +0800

    Add Files

---
+++ Func.c
@@ -0,0 +1,14 @@
+#include "Func.h"
+#define uchar unsigned char
+void Calculate(uchar c)
+{
+	
+}
+uchar read24c02(void)
+{
+	
+}
+void write24c02(void)
+{
+	
+}
---
+++ Func.h
@@ -0,0 +1,7 @@
+#ifndef _Func_
+#define _Func_
+#define uchar unsigned char
+void Calculate(uchar c);
+void write24c02(void);
+uchar read24c02(void);
+#endif
